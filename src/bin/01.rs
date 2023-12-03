8f11bc4a6f71e787ddef019c82636e2b7e9e532a
DirajHS <dirajhsdj@gmail.com>
2023-12-03 23:25:23 +0530

    Updated tests & gitignore

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -74,12 +74,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(42));
+        assert_eq!(result, Some(209));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(42));
+        assert_eq!(result, Some(198));
     }
 }
